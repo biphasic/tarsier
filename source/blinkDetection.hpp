bfb9821f7a08b24ee5e7f075f03dc3cc881fec74
Gregor Lenz <lenz.gregor@gmail.com>
2018-02-02 11:10:59 +0100

    removed event counter and detection delay

--- source/blinkDetection.hpp
+++ source/blinkDetection.hpp
@@ -20,7 +20,7 @@ public:
             std::forward<HandleBlinkEvent>(handleBlinkDetectionEvent)),
         _lifespan(lifespan), _lowerThreshold(lowerThreshold),
         _upperThreshold(upperThreshold), _lastTimeStamp(0), _xGridSize(19),
-        _yGridSize(20), _isBlink(false), _x2(0), _y2(0), _counter(0) {}
+        _yGridSize(20), _isBlink(false), _x2(0), _y2(0) {}
 
   BlinkDetection(const BlinkDetection &) = delete;
   BlinkDetection(BlinkDetection &&) = default;
@@ -29,114 +29,111 @@ public:
   virtual ~BlinkDetection() {}
 
   virtual void operator()(Event event) {
-    if (event.timestamp > 500000) {
-      _col = static_cast<int>(event.x / _xGridSize);
-      _row = static_cast<int>(event.y / _yGridSize);
+    _col = static_cast<int>(event.x / _xGridSize);
+    _row = static_cast<int>(event.y / _yGridSize);
 
-      _currentTimeStamp = event.timestamp;
-      _lastTimeStamp = _grid[_col][_row].latestTimestamp;
+    _currentTimeStamp = event.timestamp;
+    _lastTimeStamp = _grid[_col][_row].latestTimestamp;
 
-      // updating activities
-      for (size_t i = 0; i <= 15; i++) {
-        // only update tiles that are beyond the lower threshold. Values below
-        // are of no interest to an exponential decay
-        if (_grid[i][_row].activity > _lowerThreshold || i == _col) {
-          _grid[i][_row].activity *=
-              exp(-static_cast<double>(_currentTimeStamp -
-                                       _grid[i][_row].latestTimestamp) /
-                  _lifespan);
-          _grid[i][_row].latestTimestamp = _currentTimeStamp;
-          if (i == _col) {
-            // increase activity only in currently focused tile
-            _grid[i][_row].activity += 1;
+    // updating activities
+    for (size_t i = 0; i <= 15; i++) {
+      // only update tiles that are beyond the lower threshold. Values below
+      // are of no interest to an exponential decay
+      if (_grid[i][_row].activity > _lowerThreshold || i == _col) {
+        _grid[i][_row].activity *=
+            exp(-static_cast<double>(_currentTimeStamp -
+                                     _grid[i][_row].latestTimestamp) /
+                _lifespan);
+        _grid[i][_row].latestTimestamp = _currentTimeStamp;
+        if (i == _col) {
+          // increase activity only in currently focused tile
+          _grid[i][_row].activity += 1;
+        }
+
+        // state machine
+        switch (_grid[i][_row].state) {
+        // too little activity
+        case State::background:
+          // see if activity has risen above lowerThreshold to set blink
+          // candidate, state to 2
+          if (_grid[i][_row].activity > _lowerThreshold) {
+            _grid[i][_row].state = State::candidate;
+            _grid[i][_row].blinkBeginTimestamp = _currentTimeStamp;
+            std::cout << i << "/" << _row << "+" << '\n';
+          } else if (_grid[i][_row].activity > _upperThreshold) {
+            throw std::logic_error("Skipped a state");
           }
-
-          // state machine
-          switch (_grid[i][_row].state) {
-          // too little activity
-          case State::background:
-            // see if activity has risen above lowerThreshold to set blink
-            // candidate, state to 2
-            if (_grid[i][_row].activity > _lowerThreshold) {
-              _grid[i][_row].state = State::candidate;
-              _grid[i][_row].blinkBeginTimestamp = _currentTimeStamp;
-              std::cout << i << "/" << _row << "+" << '\n';
-            } else if (_grid[i][_row].activity > _upperThreshold) {
-              throw std::logic_error("Skipped a state");
-            }
-            break;
-          // within thresholds, possible blink
-          case State::candidate:
-            // check whether activity may dropped below threshold plus some
-            // safety margin
-            if (_grid[i][_row].activity < (_lowerThreshold - 10)) {
-              std::cout << i << "/" << _row << "-" << '\n';
-              _grid[i][_row].state = State::background;
-              _blinkCandidateVector[_row].push_back(std::make_pair(
-                  i, static_cast<long>((_grid[i][_row].latestTimestamp -
-                                        _grid[i][_row].blinkBeginTimestamp) /
-                                       2) +
-                         _grid[i][_row].blinkBeginTimestamp));
-            } else if (_grid[i][_row].activity > _upperThreshold) {
-              _grid[i][_row].state = State::clutter; // considered clutter now
-              std::cout << i << "/" << _row << "[++]" << '\n';
-            }
-            break;
-          // too much activity
-          case State::clutter:
-            if (_grid[i][_row].activity < _lowerThreshold) {
-              _grid[i][_row].state = State::background;
-              std::cout << i << "/" << _row << "[--]" << '\n';
-            }
-            break;
-          default:
-            // TODO refactor
+          break;
+        // within thresholds, possible blink
+        case State::candidate:
+          // check whether activity may dropped below threshold plus some
+          // safety margin
+          if (_grid[i][_row].activity < (_lowerThreshold - 10)) {
+            std::cout << i << "/" << _row << "-" << '\n';
             _grid[i][_row].state = State::background;
-            std::cout << "set state for first time. " << '\n';
-            break;
+            _blinkCandidateVector[_row].push_back(std::make_pair(
+                i, static_cast<long>((_grid[i][_row].latestTimestamp -
+                                      _grid[i][_row].blinkBeginTimestamp) /
+                                     2) +
+                       _grid[i][_row].blinkBeginTimestamp));
+          } else if (_grid[i][_row].activity > _upperThreshold) {
+            _grid[i][_row].state = State::clutter; // considered clutter now
+            std::cout << i << "/" << _row << "[++]" << '\n';
           }
+          break;
+        // too much activity
+        case State::clutter:
+          if (_grid[i][_row].activity < _lowerThreshold) {
+            _grid[i][_row].state = State::background;
+            std::cout << i << "/" << _row << "[--]" << '\n';
+          }
+          break;
+        default:
+          // TODO refactor
+          _grid[i][_row].state = State::background;
+          std::cout << "set state for first time. " << '\n';
+          break;
         }
       }
-
-      // check whether central timestamps of the last two blinking candidates
-      // (eyes) are not more than 50ms apart and candidates are two tiles apart
-      if (_blinkCandidateVector[_row].size() > 1 &&
-          abs((_blinkCandidateVector[_row].rbegin() + 1)->second -
-              _blinkCandidateVector[_row].back().second) < 50000 &&
-          (abs(((_blinkCandidateVector[_row].rbegin() + 1)->first -
-                _blinkCandidateVector[_row].back().first)) == 2
-           //||
-           // abs(((_blinkCandidateVector[_row].rbegin() + 1)->first -
-           //_blinkCandidateVector[_row].back().first)) == 1
-           )) {
-
-        _blinkVector[_row].push_back(std::make_pair(
-            static_cast<int>(
-                ((_blinkCandidateVector[_row].rbegin() + 1)->first +
-                 _blinkCandidateVector[_row].back().first) /
-                2),
-            ((_blinkCandidateVector[_row].rbegin() + 1)->second +
-             _blinkCandidateVector[_row].back().second) /
-                2));
-        if (_blinkVector[_row].size() > 1 &&
-            (_blinkVector[_row].back().first -
-             (_blinkVector[_row].rbegin() + 1)->first) < 2 &&
-            (_blinkVector[_row].back().second -
-             (_blinkVector[_row].rbegin() + 1)->second) > 800000 &&
-            (_blinkVector[_row].back().second -
-             (_blinkVector[_row].rbegin() + 1)->second) < 4000000) {
-          _isBlink = true;
-          _x2 = ((_blinkCandidateVector[_row].rbegin() + 1)->first) * 19 + 10;
-          _y2 = _row * 20 + 10;
-          std::cout << "_x2: " << _x2 << " and _y2: " << _y2 << '\n';
-          _blinkCandidateVector[_row].clear();
-        }
-      } else {
-        _isBlink = false;
-      }
-
-      _handleBlinkEvent(_blinkEventFromEvent(event, _isBlink, _x2, _y2));
     }
+
+    // check whether central timestamps of the last two blinking candidates
+    // (eyes) are not more than 50ms apart and candidates are two tiles apart
+    if (_blinkCandidateVector[_row].size() > 1 &&
+        abs((_blinkCandidateVector[_row].rbegin() + 1)->second -
+            _blinkCandidateVector[_row].back().second) < 50000 &&
+        (abs(((_blinkCandidateVector[_row].rbegin() + 1)->first -
+              _blinkCandidateVector[_row].back().first)) == 2
+         //||
+         // abs(((_blinkCandidateVector[_row].rbegin() + 1)->first -
+         //_blinkCandidateVector[_row].back().first)) == 1
+         )) {
+
+      _blinkVector[_row].push_back(std::make_pair(
+          static_cast<int>(((_blinkCandidateVector[_row].rbegin() + 1)->first +
+                            _blinkCandidateVector[_row].back().first) /
+                           2),
+          ((_blinkCandidateVector[_row].rbegin() + 1)->second +
+           _blinkCandidateVector[_row].back().second) /
+              2));
+      if (_blinkVector[_row].size() > 1 &&
+          (_blinkVector[_row].back().first -
+           (_blinkVector[_row].rbegin() + 1)->first) < 2 &&
+          (_blinkVector[_row].back().second -
+           (_blinkVector[_row].rbegin() + 1)->second) > 800000 &&
+          (_blinkVector[_row].back().second -
+           (_blinkVector[_row].rbegin() + 1)->second) < 4000000) {
+        _isBlink = true;
+        _x2 = ((_blinkCandidateVector[_row].rbegin() + 1)->first) * 19 + 10;
+        _y2 = _row * 20 + 10;
+        std::cout << "_x2: " << _x2 << " and _y2: " << _y2 << '\n';
+        _blinkCandidateVector[_row].clear();
+      }
+    } else {
+      _isBlink = false;
+    }
+
+    _handleBlinkEvent(_blinkEventFromEvent(event, _isBlink, _x2, _y2));
   }
 
 protected:
